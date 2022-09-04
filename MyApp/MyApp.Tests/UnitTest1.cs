6ae81795faef9b11261a7c3ffef08eaada6fc0b0
viggostarcke <vist@itu.dk>
2022-09-05 00:53:41 +0200

    implemented IsLeapYear, need test

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -1,14 +1,38 @@
+using System.Reflection;
+using Namespace;
+
 namespace MyApp.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void program_returns_is_leap_year_for_input_1600()
     {
-        //Arrange
+        //arrange
+        //using var writer = new StringWriter();
+        var method = t.
 
-        //Act
+        //act
+        //var program = Assembly.Load(nameof(MyApp));
+        //program.EntryPoint?.Invoke(null, new[] { "1600" });
 
-        //Assert
+        //assert
+        //var output = writer.GetStringBuilder().ToString().TrimEnd();
+        //output.Should().Be("Is leap year");
+    }
+    
+    [Fact]
+    public void program_returns_is_NOT_leap_year_for_input_1700()
+    {
+        //arrange
+        using var writer = new StringWriter();
+
+        //act
+        var program = Assembly.Load(nameof(MyApp));
+        program.EntryPoint?.Invoke(null, new[] { "1700" });
+
+        //assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Is NOT leap year");
     }
 }
--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Namespace;
+
+public class Program {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type a year and I will tell you whether it was a leap year or not");
+        string input = Console.ReadLine();
+        int year;
+        
+        if (!int.TryParse(input, out year))
+        {
+            Console.WriteLine("{0} is not an integer", input);
+        }
+        
+        if (IsLeapYear(year).Equals(true))
+        {
+            Console.WriteLine("yay");
+        }
+        else
+        {
+            Console.WriteLine("nay");
+        }
+    }
+
+    public static bool IsLeapYear(int year) 
+    {
+        if (year % 4 == 0) 
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 != 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
